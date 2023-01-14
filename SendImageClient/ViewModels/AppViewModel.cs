8ee6ce26260110db390b94ad6a527b4aade72cec
Bayr_kf60 <Bayr_kf60@STHQ012B-03>
2023-01-14 17:36:01 +0400

    Add project files.

---
+++ SendImageClient/ViewModels/AppViewModel.cs
@@ -0,0 +1,29 @@
+﻿using GalaSoft.MvvmLight.Command;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace SendImageClient.ViewModels
+{
+    public class AppViewModel:BaseViewModel
+    {
+        public SendImageClient.Commands.RelayCommand DragDropCommand { get; set; }
+        public AppViewModel()
+        {
+            DragDropCommand = new SendImageClient.Commands.RelayCommand((args) =>
+            {
+                var e = args as DragEventArgs;
+                ImagePath = null;
+                if (e.Data.GetDataPresent(DataFormats.FileDrop))
+                {
+                    string[] files = (string[])e.Data.GetData(DataFormats.FileDrop);
+                    string fileName = System.IO.Path.GetFileName(files[0]);
+                    ImagePath = files[0];
+                }
+            });
+        }
+    }
+}
