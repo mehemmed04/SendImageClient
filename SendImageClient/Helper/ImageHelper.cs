5b46e49f5829a50dc92d5b9611b42f96cc37af2f
Bayr_kf60@itstep.edu.az <Bayr_kf60@itstep.edu.az>
2023-01-17 05:37:39 -0800

    upgraded

---
+++ SendImageClient/Helper/ImageHelper.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SendImageClient.Helper
+{
+    public class ImageHelper
+    {
+        public static byte[] GetBytes(string imagePath)
+        {
+            var image = new Bitmap(imagePath);
+            ImageConverter imageconverter = new ImageConverter();
+            var imagebytes = ((byte[])imageconverter.ConvertTo(image, typeof(byte[])));
+            return imagebytes;
+        }
+    }
+}
--- SendImageClient/ViewModels/AppViewModel.cs
+++ SendImageClient/ViewModels/AppViewModel.cs
@@ -1,19 +1,60 @@
 ﻿using GalaSoft.MvvmLight.Command;
+using SendImageClient.Commands;
+using SendImageClient.Helper;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
+using System.Net.Sockets;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
 
 namespace SendImageClient.ViewModels
 {
-    public class AppViewModel:BaseViewModel
+    public class AppViewModel : BaseViewModel
     {
+
+        Socket socket = null;
+        IPAddress ipAddress = null;
+        int port = 80;
         public SendImageClient.Commands.RelayCommand DragDropCommand { get; set; }
+        private string imagePath;
+
+        public string ImagePath
+        {
+            get { return imagePath; }
+            set { imagePath = value; OnPropertyChanged(); }
+        }
+
+        public void ConnectServer()
+        {
+            socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            ipAddress = IPAddress.Parse(IPHelper.GetLocalIpAddress());
+
+            var ep = new IPEndPoint(ipAddress, port);
+            //var bytes = new byte[50000];
+
+            try
+            {
+                socket.Connect(ep);
+                if (socket.Connected)
+                {
+                    MessageBox.Show("Connected");
+                }
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Can not connect to the server . . .");
+            }
+        }
+        public Commands.RelayCommand ConnectCommand { get; set; }
+        public Commands.RelayCommand SendImageCommand { get; set; }
+
         public AppViewModel()
         {
-            DragDropCommand = new SendImageClient.Commands.RelayCommand((args) =>
+            DragDropCommand = new Commands.RelayCommand((args) =>
             {
                 var e = args as DragEventArgs;
                 ImagePath = null;
@@ -24,6 +65,24 @@ namespace SendImageClient.ViewModels
                     ImagePath = files[0];
                 }
             });
+
+
+            SendImageCommand = new Commands.RelayCommand((o) =>
+            {
+                Task.Run(() =>
+                {
+                    ImageHelper imageHelper = new ImageHelper();
+                    var imagebytes = ImageHelper.GetBytes(ImagePath);
+                    socket.Send(imagebytes);
+                    ImagePath = null;
+                });
+            });
+
+            ConnectCommand = new Commands.RelayCommand((o) =>
+            {
+                ConnectServer();
+            });
+
         }
     }
 }
